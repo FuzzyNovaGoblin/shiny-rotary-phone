9dd6ee1614a38388397f507cd7e2c382a365178e
Redempt <instantiable@gmail.com>
2022-12-03 13:35:24 -0500

    Add databind

--- app/src/main/java/com/example/shiny_rotary_phone/MainActivity.kt
+++ app/src/main/java/com/example/shiny_rotary_phone/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        com.example.shiny_rotary_phone.d
     }
 }
